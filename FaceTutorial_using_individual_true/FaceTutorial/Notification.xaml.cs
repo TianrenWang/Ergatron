a4d96d99cd2d0d2650bb3a67ffa4178e580292ac
Brendan J <johnstonbrendan111@gmail.com>
2018-02-26 11:08:32 -0500

    Create the spereate projects for when one of the individual selections are true, or when both are true. Referring to the frustration and tiredness choices

---
+++ FaceTutorial_using_individual_true/FaceTutorial/Notification.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FaceTutorial
+{
+    /// <summary>
+    /// Interaction logic for Notification.xaml
+    /// </summary>
+    public partial class Notification : Window
+    {
+        public Notification()
+        {
+            InitializeComponent();
+            //notification.Content = "Watch your proximity to the screen";
+        }
+
+        public Notification(int emotion)
+        {
+            InitializeComponent();
+            switch (emotion)
+            {
+                case 0:
+                    notification.Content = "You're both tired and frustrated!";
+                    break;
+                case 1:
+                    notification.Content = "Time to sleep, you are tired!";
+                    break;
+                case 2:
+                    notification.Content = "You are frustrated, take a breather!";
+                    break;
+                           
+            }
+        }
+    }
+}
---
+++ FaceTutorial_using_individual_true/FaceTutorial/Window1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FaceTutorial
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public static int count;
+        public Window1()
+        {
+            this.InitializeComponent();
+            count = 0;
+            ChangeLabel();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            count++;
+            ChangeLabel();
+        }
+
+        private void ChangeLabel()
+        {
+            switch (count)
+            {
+                case 0:
+                    Command.Content = "Welcome! Click the button once you are ready to calibrate your facial features!";
+                    break;
+                case 1:
+                    Command.Content = "Take a picture with your eyes open.";
+                    break;
+                case 2:
+                    Command.Content = "Squint your eyes and take a picture.";
+                    break;
+                case 3:
+                    Command.Content = "Thank you! Your facial pictures are calibrated.";
+                    //CameraVideoDeviceControl.Visibility = Visibility.Hidden;
+                    btnOk.Visibility = Visibility.Hidden;
+                    break;
+            }
+        }
+
+        private void Window_IsStylusCapturedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
